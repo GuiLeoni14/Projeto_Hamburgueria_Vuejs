cde5fec1d6eae5202f77982dd243bfb84889ad32
GuiLeoni14 <guileone167@gmail.com>
2021-11-29 18:56:33 -0200

    criando banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="main_banner">
+        <div class="container">
+            <h1>Make Your Burguer</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+<style lang="scss" scoped>
+    .main_banner{
+        background: url("/img/burger.jpg") no-repeat center center;
+        background-size: cover;
+        background-position: 0rem -25rem;
+        height: 50rem;
+        .container{
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            height: inherit;
+            h1{
+                color: #fff;
+                font-size: 4.5rem;
+                background-color: #222;
+                border-radius: 0.5rem;
+                padding: 2rem 4rem;
+            }
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
     <section class="s-home">
+        <Banner/>
         <div class="container">
-            <h1>Home</h1>
+            <h1>Monte o seu Burguer: </h1>
         </div>
     </section>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
 // @ is an alias to /src
 export default {
     name: 'Home',
+    components: {
+        Banner,
+    }
 }
 </script>
 
